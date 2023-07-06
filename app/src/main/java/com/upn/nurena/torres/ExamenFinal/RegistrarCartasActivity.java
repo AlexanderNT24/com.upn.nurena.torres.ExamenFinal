e4836ca0788c823fe9643a765292f88c01891480
Alexander Nureña <alexander26nt@gmail.com>
2023-07-06 11:31:48 -0500

    get image form galery

--- app/src/main/java/com/upn/nurena/torres/ExamenFinal/RegistrarCartasActivity.java
+++ app/src/main/java/com/upn/nurena/torres/ExamenFinal/RegistrarCartasActivity.java
@@ -1,8 +1,14 @@
 package com.upn.nurena.torres.ExamenFinal;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +19,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.upn.nurena.torres.ExamenFinal.helpers.DatabaseHelper;
 import com.upn.nurena.torres.ExamenFinal.entities.Carta;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class RegistrarCartasActivity extends AppCompatActivity {
 
     private EditText etNombreCarta;
@@ -21,6 +31,10 @@ public class RegistrarCartasActivity extends AppCompatActivity {
     private Button btnRegistrarCarta;
 
     private DatabaseHelper databaseHelper;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Uri imagenUri;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +54,14 @@ public class RegistrarCartasActivity extends AppCompatActivity {
                 registrarCarta();
             }
         });
+
+        Button btnAbrirGaleria = findViewById(R.id.btn_abrir_galeria);
+        btnAbrirGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirGaleria();
+            }
+        });
     }
 
     private void registrarCarta() {
@@ -51,7 +73,9 @@ public class RegistrarCartasActivity extends AppCompatActivity {
             float puntosAtaque = Float.parseFloat(puntosAtaqueStr);
             float puntosDefensa = Float.parseFloat(puntosDefensaStr);
 
-            Carta carta = new Carta(1,nombreCarta,2, (int) puntosAtaque, (int) puntosDefensa,"",1,1);
+            String imagenBase64 = obtenerImagenBase64();
+
+            Carta carta = new Carta(1, nombreCarta, 2, (int) puntosAtaque, (int) puntosDefensa, imagenBase64, 1, 1);
 
             long resultado = insertarCartaEnBD(carta);
 
@@ -67,15 +91,43 @@ public class RegistrarCartasActivity extends AppCompatActivity {
     }
 
     private long insertarCartaEnBD(Carta carta) {
-
         return 1;
     }
-
 
     private void limpiarCampos() {
         etNombreCarta.setText("");
         etPuntosAtaque.setText("");
         etPuntosDefensa.setText("");
     }
-}
 
+    private void abrirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            imagenUri = data.getData();
+        }
+    }
+
+    private String obtenerImagenBase64() {
+        if (imagenUri != null) {
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(imagenUri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, byteArrayOutputStream);
+                byte[] byteArray = byteArrayOutputStream.toByteArray();
+                return Base64.encodeToString(byteArray, Base64.DEFAULT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return ""; // Devuelve una cadena vacía en caso de error
+    }
+
+}
